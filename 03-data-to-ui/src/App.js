16d3425b4cdfb2eb02bdd2b2403a7efd2e770699
Verónica García Pulido <verogarp@gmail.com>
2020-01-29 12:18:18 +0000

    Add more Circles with random colors

--- 03-data-to-ui/src/App.js
+++ 03-data-to-ui/src/App.js
@@ -3,9 +3,19 @@ import "./App.css";
 import Circle from "./components/Circle";
 
 function App() {
+  function showCircle() {
+    var colors = ["#393E41", "#E94F37", "#1C89BF", "#A1D363"];
+    var ran = Math.floor(Math.random() * colors.length);
+
+    // return a Circle with a randomly chosen color
+    return <Circle bgColor={colors[ran]} />;
+  }
+
   return (
     <div>
-      <Circle bgColor="#F9C240" />
+      {showCircle()}
+      {showCircle()}
+      {showCircle()}
     </div>
   );
 }
